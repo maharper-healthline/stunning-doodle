843e5a1230218b486e5529278e97e819cddd623c
maharper-healthline <60898476+maharper-healthline@users.noreply.github.com>
2020-12-03 14:59:34 -0800

    Update transform revenue_combined table for marketplace dash.sql

--- transform revenue_combined table for marketplace dash.sql
+++ transform revenue_combined table for marketplace dash.sql
@@ -1,8 +1,14 @@
 SELECT 
-  *
+  page_path, 
+  cohort, 
+  k1, 
+  month, 
+  site, 
+  revenue_type, 
+  amount
 FROM `bigquery-1084.content_roi.revenue_combined`
 WHERE  
-  month >= DATE_SUB(CURRENT_DATE(), INTERVAL 18 MONTH)
+  month >= DATE_SUB(CURRENT_DATE(), INTERVAL 13 MONTH)
   
 UNION ALL 
 
@@ -10,18 +16,15 @@ SELECT
   a.pagepath, 
   DATE('9999-01-01') AS cohort, 
   k.k1, 
-  k2, 
   DATE_TRUNC(datept, month) AS month, 
   site, 
-  '' as type, 
-  '' as team, 
   'secondsonsite' as revenue_type, 
   sum(secondsonsite) as amount
 FROM `bigquery-1084.maharper.traffic_union` a
 LEFT JOIN `bigquery-1084.gino.pagepath_k1_clean_mapping` k
   ON REGEXP_REPLACE(k.pagepath, ".php","") = REGEXP_REPLACE(a.pagepath, ".php","")
 GROUP BY 
-  1, 2, 3, 4, 5, 6, 7, 8, 9 
+  1, 2, 3, 4, 5, 6
   
 UNION ALL 
 
@@ -29,15 +32,12 @@ SELECT
   a.pagepath, 
   DATE('9999-01-01') AS cohort, 
   k.k1, 
-  k2, 
   DATE_TRUNC(datept, month) AS month, 
   site, 
-  '' as type, 
-  '' as team, 
   'bounces' as revenue_type, 
   sum(bounce) as amount
 FROM `bigquery-1084.maharper.traffic_union` a
 LEFT JOIN `bigquery-1084.gino.pagepath_k1_clean_mapping` k
   ON REGEXP_REPLACE(k.pagepath, ".php","") = REGEXP_REPLACE(a.pagepath, ".php","")
 GROUP BY 
-  1, 2, 3, 4, 5, 6, 7, 8, 9
+  1, 2, 3, 4, 5, 6

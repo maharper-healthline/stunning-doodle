924feaca2e10361a969b10c81b1bcc0ec300d99d
maharper-healthline <60898476+maharper-healthline@users.noreply.github.com>
2021-09-27 17:43:27 -0700

    fixed bad join
    forgot to have month/year level of detail in traffic subquery. also aggregated by page_path rather than post_id for traffic subquery

--- url level revenue data for katrina.sql
+++ url level revenue data for katrina.sql
@@ -1,29 +1,34 @@
 WITH traffic AS (
                 SELECT
                     site
-                    , post_id
-                    , SUM(session) AS sessions
+                    , RTRIM(page_path, '/')                                             AS page_path
+                    , TRUNC(DATE_TRUNC('month', TO_DATE(date_pt_sk::text, 'YYYYMMDD'))) AS month_year
+                    , SUM(session)                                                      AS sessions
                 FROM hl.reporting.mv_traffic
                 WHERE
-                    date_pt_sk BETWEEN 20210701 AND 20210731
-                GROUP BY 1, 2
+                    date_pt_sk BETWEEN 20210101 AND 20210731
+                GROUP BY 1, 2, 3
                 ),
     revenue  AS (
                 SELECT
                     site
                     , page_path
-                    , SUM(CASE WHEN revenue_category IN ('Activation', 'Amazon Affiliates') THEN estimated_revenue END)         AS activation_rev
+                    , TRUNC(month_year)                                                                 AS month_year
+                    , SUM(CASE
+                              WHEN revenue_category IN ('Activation', 'Amazon Affiliates') THEN estimated_revenue
+                          END)                                                                          AS activation_rev
                     , SUM(CASE WHEN revenue_category LIKE '%DAS%' THEN estimated_revenue END)           AS das_rev
                     , SUM(CASE
                               WHEN revenue_category ILIKE '%programmatic%' THEN estimated_revenue
                           END)                                                                          AS programmatic_rev
                 FROM hl.reporting.mv_estimated_revenue_combined
                 WHERE
-                    TRUNC(month_year) = '2021-07-01'
-                GROUP BY 1, 2
+                    TRUNC(month_year) BETWEEN '2021-01-01' AND '2021-07-01'
+                GROUP BY 1, 2, 3
                 )
 SELECT
-    wf.post_id
+    t.month_year
+    , wf.post_id
     , wf.website
     , RTRIM(wf.page_path, '/') AS page_path
     , wf.meta_title
@@ -40,6 +45,7 @@ SELECT
     , rev.programmatic_rev
 FROM hl.wordpress.wp_workflow wf
     INNER JOIN traffic t
-               ON LOWER(wf.table_id) = LOWER(t.site) AND wf.post_id = REGEXP_SUBSTR(t.post_id, '\\d+')
+               ON LOWER(wf.table_id) = LOWER(t.site) AND RTRIM(wf.page_path, '/') = RTRIM(t.page_path, '/')
     INNER JOIN revenue rev
-               ON LOWER(wf.table_id) = LOWER(rev.site) AND RTRIM(wf.page_path, '/') = RTRIM(rev.page_path, '/')
+               ON LOWER(wf.table_id) = LOWER(rev.site) AND RTRIM(wf.page_path, '/') = RTRIM(rev.page_path, '/') AND
+                  t.month_year = rev.month_year
